a7307b9d550419b20bb39b8858d92febc2477a6f
Stefan Davis <stefan@turnkeylinux.org>
2019-06-26 03:22:49 +0000

    Migrated from turnkey-pylib

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='turnkey-temp',
+    version='1.0',
+    description='A small temp file library',
+    author='Stefan Davis',
+    url='https://github.com/turnkeylinux/turnkey-temp',
+    packages=['temp'])
